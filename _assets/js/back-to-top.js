15fb36da8a019af955baf17a42dd5b5288c316f7
Jacob Bearden <jacob@bearden.io>
2019-07-31 16:31:47 -0700

    fix script-src issue

---
+++ _assets/js/back-to-top.js
@@ -0,0 +1,13 @@
+const element = document.getElementById('back-to-top');
+
+window.onscroll = () => {
+  if (window.pageYOffset > 190) {
+    element.style.display =  'block';
+    return;
+  }
+  element.style.display =  'none';
+};
+
+element.onclick = () => {
+  window.scrollTo(0, 0);
+};
